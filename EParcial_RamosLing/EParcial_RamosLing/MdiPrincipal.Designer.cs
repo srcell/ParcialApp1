7c6951786029ed4afb426300db759bdaa9fb4074
italo <italodavidramosling@gmail.com>
2022-10-23 15:36:41 -0500

    Se culmino MDIPrincipal, solo falta colocar iconos [de ser necesario]

--- EParcial_RamosLing/EParcial_RamosLing/MdiPrincipal.Designer.cs
+++ EParcial_RamosLing/EParcial_RamosLing/MdiPrincipal.Designer.cs
@@ -30,13 +30,18 @@
         {
             this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.seleccioneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.empleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.transportistaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirDelAplicativoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblUsuario = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblSesion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -49,25 +54,14 @@
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
-            this.statusStrip1.TabIndex = 1;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 1000;
-            // 
             // seleccioneToolStripMenuItem
             // 
             this.seleccioneToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.clienteToolStripMenuItem,
             this.empleadoToolStripMenuItem,
-            this.transportistaToolStripMenuItem});
+            this.transportistaToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.salirDelAplicativoToolStripMenuItem});
             this.seleccioneToolStripMenuItem.Name = "seleccioneToolStripMenuItem";
             this.seleccioneToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
             this.seleccioneToolStripMenuItem.Text = "--Seleccione--";
@@ -75,23 +69,67 @@
             // clienteToolStripMenuItem
             // 
             this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
-            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
             this.clienteToolStripMenuItem.Text = "Cliente";
             this.clienteToolStripMenuItem.Click += new System.EventHandler(this.clienteToolStripMenuItem_Click);
             // 
             // empleadoToolStripMenuItem
             // 
             this.empleadoToolStripMenuItem.Name = "empleadoToolStripMenuItem";
-            this.empleadoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.empleadoToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
             this.empleadoToolStripMenuItem.Text = "Empleado";
             this.empleadoToolStripMenuItem.Click += new System.EventHandler(this.empleadoToolStripMenuItem_Click);
             // 
             // transportistaToolStripMenuItem
             // 
             this.transportistaToolStripMenuItem.Name = "transportistaToolStripMenuItem";
-            this.transportistaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.transportistaToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
             this.transportistaToolStripMenuItem.Text = "Transportista";
             this.transportistaToolStripMenuItem.Click += new System.EventHandler(this.transportistaToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(166, 6);
+            // 
+            // salirDelAplicativoToolStripMenuItem
+            // 
+            this.salirDelAplicativoToolStripMenuItem.Name = "salirDelAplicativoToolStripMenuItem";
+            this.salirDelAplicativoToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.salirDelAplicativoToolStripMenuItem.Text = "Salir del aplicativo";
+            this.salirDelAplicativoToolStripMenuItem.Click += new System.EventHandler(this.salirDelAplicativoToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblUsuario,
+            this.lblSesion});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(47, 17);
+            this.lblUsuario.Text = "Equipo:";
+            // 
+            // lblSesion
+            // 
+            this.lblSesion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSesion.Name = "lblSesion";
+            this.lblSesion.Size = new System.Drawing.Size(44, 17);
+            this.lblSesion.Text = "Sesion:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // MdiPrincipal
             // 
@@ -101,11 +139,17 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MdiPrincipal";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MdiPrincipal_Load);
+            this.Resize += new System.EventHandler(this.MdiPrincipal_Resize);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,5 +164,9 @@
         private ToolStripMenuItem transportistaToolStripMenuItem;
         private StatusStrip statusStrip1;
         private System.Windows.Forms.Timer timer1;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem salirDelAplicativoToolStripMenuItem;
+        private ToolStripStatusLabel lblUsuario;
+        private ToolStripStatusLabel lblSesion;
     }
 }
--- EParcial_RamosLing/EParcial_RamosLing/MdiPrincipal.cs
+++ EParcial_RamosLing/EParcial_RamosLing/MdiPrincipal.cs
@@ -41,32 +41,69 @@ namespace EParcial_RamosLing
 
         private void empleadoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form frm1 = Application.OpenForms.Cast<Form>().FirstOrDefault(x => x is frmCrudEmpleado);
-            if (frm1 != null)
+            Form frm2 = Application.OpenForms.Cast<Form>().FirstOrDefault(x => x is frmCrudEmpleado);
+            if (frm2 != null)
             {
-                frm1.BringToFront();
+                frm2.BringToFront();
                 return;
             }
 
             //Si no existe un formulario, se abre uno
-            frm1 = new frmCrudEmpleado();
-            frm1.MdiParent = this;
-            frm1.Show();
+            frm2 = new frmCrudEmpleado();
+            frm2.MdiParent = this;
+            frm2.Show();
         }
 
         private void transportistaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form frm1 = Application.OpenForms.Cast<Form>().FirstOrDefault(x => x is frmCrudTransportista);
-            if (frm1 != null)
+            Form frm3 = Application.OpenForms.Cast<Form>().FirstOrDefault(x => x is frmCrudTransportista);
+            if (frm3 != null)
             {
-                frm1.BringToFront();
+                frm3.BringToFront();
                 return;
             }
 
             //Si no existe un formulario, se abre uno
-            frm1 = new frmCrudTransportista();
-            frm1.MdiParent = this;
-            frm1.Show();
+            frm3 = new frmCrudTransportista();
+            frm3.MdiParent = this;
+            frm3.Show();
+        }
+
+        private void salirDelAplicativoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MdiPrincipal_Resize(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Sistemas Villaze - " + DateTime.Now;
+
+            lblSesion.Text = "Tiempo de sesion: " +
+                DateTime.Now.TimeOfDay.Subtract(horaEntrada).ToString().Substring(0, 8);
+        }
+
+        private void MdiPrincipal_Load(object sender, EventArgs e)
+        {
+            //Guardamos la hora en que se carga el formulario
+            horaEntrada = DateTime.Now.TimeOfDay;
+
+            //obtenemos informacion del equipo
+            if (miComputadora.Network.IsAvailable == true)
+            {
+                mired = "Equipo con conexion a red disponible";
+            }
+            else
+            {
+                mired = "Equipo no cuenta con conexion a red disponible";
+            }
+
+            //Mostramosa informacion en lblUsuario
+            lblUsuario.Text = "Equipo: " + miComputadora.Name + " - " + mired;
         }
     }
 }
