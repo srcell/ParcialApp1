264e71f0f4a79c9c231b18cd475e37243a2665d7
BudgetUnzueta <116515214+BudgetUnzueta@users.noreply.github.com>
2022-10-24 04:50:06 -0500

    Add files via upload

---
+++ EParcial_RamosLing/ProyVillaze_BL/TransportistaBL.cs
@@ -0,0 +1,39 @@
+﻿using ProyVillaze_ADO;
+using ProyVillaze_BE;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Agregar...
+
+namespace ProyVillaze_BL
+{
+    public  class TransportistaBL
+    {
+        TransportistaADO objTransportistaADO = new TransportistaADO();
+
+        public DataTable ListarTransportista()
+        { 
+           return objTransportistaADO.ListarTransportista();
+        }
+        public TransportistaBE ConsultarTransportista(String strCodigo)
+        {
+            return objTransportistaADO.ConsultarTransportista(strCodigo );
+        }
+
+        public Boolean InsertarTransportista(TransportistaBE objTransportistaBE)
+        {
+            return objTransportistaADO.InsertarTransportista(objTransportistaBE);
+        }
+        public Boolean ActualizarTransportista(TransportistaBE objTransportistaBE)
+        {
+            return objTransportistaADO.ActualizarTransportista(objTransportistaBE);
+        }
+        public Boolean EliminarTransportista(String strCodigo)
+        {
+            return objTransportistaADO.EliminarTransportista(strCodigo);
+        }
+    }
+}
