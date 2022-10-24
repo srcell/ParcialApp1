040bae5a61c4cb4a1e989a944053e511fb2b8c84
italo <italodavidramosling@gmail.com>
2022-10-24 02:31:34 -0500

    solucionadno erroes

--- EParcial_RamosLing/EParcial_RamosLing/Program.cs
+++ EParcial_RamosLing/EParcial_RamosLing/Program.cs
@@ -11,7 +11,7 @@ namespace EParcial_RamosLing
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MdiPrincipal());
+            Application.Run(new frmLogin());
         }
     }
 }
